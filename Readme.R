11393b19479e7543fb67ff0e6685a1a3d5387df8
Péter Kalicz <kaliczp@gmail.com>
2022-10-07 09:55:59 +0200

    Fully refactor to the function

--- Readme.R
+++ Readme.R
@@ -1,47 +1,29 @@
 library(readxl)
-ggdotchart <- as.data.frame(read_excel("ggdotchart.xlsx"))
-## lollipop_chart
+corn <- as.data.frame(read_excel("ggdotchartall.xlsx"))
 library(ggplot2)
-library(ggcharts)
 
-diverging_lollipop_chart(
-    data = ggdotchart, x = Country, y = Wheat,                              
-    lollipop_colors = c("darkgreen", "darkred"),
-    line_size = 0.75,
-    point_size = 3,
-    text_color = c("darkgreen", "darkred"),
-    text_size = 10)
+SimpleLolly <- function(x, variable){
+    ## Delete NA rows in varialble
+    x <- x[!is.na(x[, variable]),]
+    ## Order variables
+    x <- x[order(x[, variable]), ]
+    ## Labels in the right order
+    x$Country <- factor(x$Country, levels = x$Country)
+    ## Colour by sing
+    colourbysign <- ifelse(x[, variable] > 0, "darkgreen", "darkred")
 
-ggdotchart$Country <- factor(ggdotchart$Country, levels = ggdotchart$Country)
+    out.loll <- ggplot(data = x, aes(x=Country, y=.data[[variable]])) +
+        geom_segment( aes(xend=Country, yend=0), colour= colourbysign) +
+        geom_point( size=3, colour = colourbysign) +
+        coord_flip() +
+        theme_bw() +
+        theme(axis.text.y = element_text(colour = colourbysign))
 
-colourbysign <- ifelse(ggdotchart$Wheat > 0, "darkgreen", "darkred")
+    out.loll
+}
 
-wheat.loll <- ggplot(data = ggdotchart, aes(x=Country, y=Wheat)) +
-    geom_segment( aes(xend=Country, yend=0), colour= colourbysign) +
-    geom_point( size=3, colour = colourbysign) +
-    coord_flip() +
-    theme_bw() +
-    theme(axis.text.y = element_text(colour = colourbysign))
-
-
-maize <- as.data.frame(read_excel("ggdotchart.xlsx", sheet = 2))
-
-diverging_lollipop_chart(
-    data = maize, x = Country, y = Maize,
-    lollipop_colors = c("darkgreen", "darkred"),
-    line_size = 0.75,
-    point_size = 3,
-    text_color = c("darkgreen", "darkred"),
-    text_size = 10)
-
-maize$Country <- factor(maize$Country, levels = maize$Country)
-
-maize.loll <- ggplot(data = maize, aes(x=Country, y=Maize)) +
-    geom_segment( aes(xend=Country, yend=0), color="darkred") +
-    geom_point( size=3, color="darkred") +
-    coord_flip() +
-    theme_bw() +
-    theme(axis.text.y = element_text(colour = "darkred"))
+wheat.loll <- SimpleLolly(corn, variable = "Wheat")
+maize.loll <- SimpleLolly(corn, variable = "Maize")
 
 pdf()
 wheat.loll
