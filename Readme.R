3de46ff0f012869f403cbb36a6e6c8d4b5ebc5e0
Péter Kalicz <kaliczp@gmail.com>
2022-10-08 08:56:36 +0200

    y axis moved to 0 value

--- Readme.R
+++ Readme.R
@@ -14,12 +14,26 @@ SimpleLolly <- function(x, variable){
 
     out.loll <- ggplot(data = x, aes(x=Country, y=.data[[variable]])) +
         geom_segment( aes(xend=Country, yend=0), colour= colourbysign) +
+        geom_hline(yintercept=0, colour = "darkgrey") +
         geom_point( size=3, colour = colourbysign) +
         coord_flip() +
         theme_classic() +
-        theme(axis.text.y = element_text(colour = colourbysign),
+        labs(x = NULL) +
+        guides(y = "none") +
+        theme(
               panel.border = element_blank())
-
+    pos.lab <- subset(x, get(variable) >= 0)
+    if(nrow(pos.lab) > 0) {
+        out.loll <- out.loll + geom_text(aes(y = -1, label = Country, hjust = "right"),
+                                         data = pos.lab,
+                                         colour = "darkgreen")
+    }
+    neg.lab <- subset(x, get(variable) < 0)
+    if(nrow(neg.lab) > 0){
+        out.loll <- out.loll + geom_text(aes(y = 1, label = Country, hjust = "left"),
+                                         data = neg.lab,
+                                         colour = "darkred")
+    }
     out.loll
 }
 
