c47c1ef007e50742a24780dbb04cbddcb5b11599
Péter Kalicz <kaliczp@gmail.com>
2022-10-08 09:27:07 +0200

    Solve label cropping problem
    To apply clip off it was neccessary to remove cord_flip() so y and x everywhere is
    changed. Now both only-positive and only-negative values are ok.

--- Readme.R
+++ Readme.R
@@ -12,27 +12,36 @@ SimpleLolly <- function(x, variable){
     ## Colour by sing
     colourbysign <- ifelse(x[, variable] > 0, "darkgreen", "darkred")
 
-    out.loll <- ggplot(data = x, aes(x=Country, y=.data[[variable]])) +
-        geom_segment( aes(xend=Country, yend=0), colour= colourbysign) +
-        geom_hline(yintercept=0, colour = "darkgrey") +
-        geom_point( size=3, colour = colourbysign) +
-        coord_flip() +
+    out.loll <- ggplot(data = x, aes(x=.data[[variable]], y=Country)) +
+        geom_segment( aes(yend=Country, xend=0), colour= colourbysign) +
+        geom_vline(xintercept=0, colour = "darkgrey") +
+        geom_point(size=3, colour = colourbysign) +
         theme_classic() +
-        labs(x = NULL) +
+        labs(y = NULL) +
         guides(y = "none") +
         theme(
               panel.border = element_blank())
     pos.lab <- subset(x, get(variable) >= 0)
+    neg.lab <- subset(x, get(variable) < 0)
     if(nrow(pos.lab) > 0) {
-        out.loll <- out.loll + geom_text(aes(y = -1, label = Country, hjust = "right"),
+        out.loll <- out.loll + geom_text(aes(x = -1, label = Country, hjust = "right"),
                                          data = pos.lab,
                                          colour = "darkgreen")
+                if(nrow(neg.lab) == 0) {
+            out.loll <- out.loll +
+                theme(plot.margin = margin(0, 0, 0, 1.5, 'cm')) +
+                coord_cartesian(clip = "off")
+            }
     }
-    neg.lab <- subset(x, get(variable) < 0)
     if(nrow(neg.lab) > 0){
-        out.loll <- out.loll + geom_text(aes(y = 1, label = Country, hjust = "left"),
+        out.loll <- out.loll + geom_text(aes(x = 1, label = Country, hjust = "left"),
                                          data = neg.lab,
                                          colour = "darkred")
+        if(nrow(pos.lab) == 0) {
+            out.loll <- out.loll +
+                theme(plot.margin = margin(0, 1.5, 0, 0, 'cm')) +
+                coord_cartesian(clip = "off")
+            }
     }
     out.loll
 }
