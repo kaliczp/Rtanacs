d71674c906dc437917a9170d68113c74e83164de
Péter Kalicz <kaliczp@gmail.com>
2022-10-25 07:14:45 +0200

    Missing data optionally droppable
    Argument na.rm drops the missing records from plot (database).

--- ReorderedLolly.R
+++ ReorderedLolly.R
@@ -2,7 +2,9 @@ corn.col$CCCode <- paste(corn.col$Country, corn.col$Corn, sep = "__")
 corn.col$CCCode <- factor(corn.col$CCCode, levels = corn.col[order(corn.col$Perc), "CCCode"])
 
 
-ReorderedLolly <- function(x, variable) {
+ReorderedLolly <- function(x, variable, na.rm = FALSE) {
+    if(na.rm)
+        x <- x[!is.na(x[, variable]),]
     colourbysign <- ifelse(x[, variable] > 0, "darkgreen", "darkred")
     fontcolour <- "white"
     cornlabel <- round(x[, variable])
@@ -19,7 +21,7 @@ ReorderedLolly <- function(x, variable) {
     out
 }
 
-ReorderedLolly(corn.col, "Perc")
+ReorderedLolly(corn.col, "Perc", na.rm = TRUE)
 
 pdf(paste0("GgdotchartBuborékban_rank6.pdf"), height = 27 / 2.54, width = 18 / 2.54)
 ReorderedLolly(corn.col, "Perc")
