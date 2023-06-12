dfdc3a7227c130f9d5a6b77069175b3e248006f7
Péter Kalicz <kaliczp@gmail.com>
2023-06-12 14:55:31 +0200

    First draft of income lollypop

--- reviewre.R
+++ reviewre.R
@@ -5,3 +5,7 @@ eur.df <- data.frame(Country = eur.raw[,1, drop = TRUE],
                      Income = eur.raw[,2, drop = TRUE],
                      Corn = "EUR"
                      )
+eur.df$CCCode <- paste0(eur.df$Country, "__EUR")
+eur.df$CCCode <- factor(eur.df$CCCode, levels = eur.df[nrow(eur.df):1, "CCCode"])
+
+ReorderedLolly(eur.df, "Income", yaxis = FALSE)
