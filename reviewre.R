eb31b9e5517ac3f3de3d1905d1a1f7989fc2a6ad
Péter Kalicz <kaliczp@gmail.com>
2023-06-12 14:20:14 +0200

    Initial database created

--- reviewre.R
+++ reviewre.R
@@ -1,2 +1,7 @@
 library(readxl)
 eur.raw <- read_excel("Reviewre/fig_3.xlsx")
+
+eur.df <- data.frame(Country = eur.raw[,1, drop = TRUE],
+                     Income = eur.raw[,2, drop = TRUE],
+                     Corn = "EUR"
+                     )
