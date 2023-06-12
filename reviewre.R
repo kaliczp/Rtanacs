d2b97aad61c81b8c9c95b50013c92529e1a3c6b0
Péter Kalicz <kaliczp@gmail.com>
2023-06-12 17:16:12 +0200

    Color and label tresholds are modified

--- reviewre.R
+++ reviewre.R
@@ -10,6 +10,6 @@ eur.df$CCCode <- factor(eur.df$CCCode, levels = eur.df[nrow(eur.df):1, "CCCode"]
 
 pdf("GgdotchartEUR.pdf", height = 11.25 / 2.54, width = 6 / 2.54, pointsize = 5)
 ReorderedLolly(eur.df, "Income", yaxis = FALSE,
-               shape = "rectangle",
-               close.lab.threshold = 220, colse.lab.factor = 1.05)
+               shape = "rectangle", col = c("#5d6683", "#af7f6b"),
+               close.lab.threshold = 250, colse.lab.factor = 1.005)
 dev.off()
