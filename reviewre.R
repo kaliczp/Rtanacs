dde991ae631a2766eefee1eedda5187f1314a08e
Péter Kalicz <kaliczp@gmail.com>
2023-06-22 18:02:10 +0200

    EUR label corrected

--- reviewre.R
+++ reviewre.R
@@ -3,7 +3,7 @@ eur.raw <- read_excel("Reviewre/fig_3.xlsx")
 
 eur.df <- data.frame(Country = eur.raw[,1, drop = TRUE],
                      Income = eur.raw[,2, drop = TRUE],
-                     Corn = "EUR"
+                     Corn = "Million EUR"
                      )
 eur.df$CCCode <- paste0(eur.df$Country, "__EUR")
 eur.df$CCCode <- factor(eur.df$CCCode, levels = eur.df[nrow(eur.df):1, "CCCode"])
