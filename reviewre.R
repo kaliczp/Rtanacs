b12eaf4928366d127bca7e37e5a7fdb1beba875c
Péter Kalicz <kaliczp@gmail.com>
2023-10-05 18:12:45 +0200

    Title removed and annotation added at left

--- reviewre.R
+++ reviewre.R
@@ -19,5 +19,8 @@ dev.off()
 pdf("GgdotchartEURff.pdf", height = 11.25 / 2.54, width = 6 / 2.54, pointsize = 5)
 ReorderedLolly(eur.df, "Income", yaxis = FALSE, xlim = c(-3360, 1440),
                shape = "rectangle", col = c("darkgray", "black"),
-               lab.dist.default = 90, close.lab.threshold = 250, colse.lab.factor = 1.2)
+               lab.dist.default = 90, close.lab.threshold = 250, colse.lab.factor = 1.2) +
+    theme( strip.background = element_blank(),
+          strip.text.x = element_blank()) +
+    annotate("text", label = "millió EUR", size = 4, x = -3360, y = 14, angle = 90)
 dev.off()
